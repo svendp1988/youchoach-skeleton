fbf11c6434e8ff116c7a1cf270c6b453367188cd
Sven De Potter <sven.depotter@hotmail.com>
2020-05-20 15:39:57 +0200

    messing around with cypress

--- youcoach-frontend/cypress/integration/sample_spec.js
+++ youcoach-frontend/cypress/integration/sample_spec.js
@@ -56,3 +56,45 @@ describe('The Login Page', () => {
     // cy.get('h1').should('contain', 'jane.lane')
   })
 })
+
+describe('The Home Page', () => {
+  it('successfully loads', () => {
+    cy.visit('/');
+
+    cy.get('a[routerLink*="/login"]').contains('Sign').click();
+
+    cy.get('input[id=username]').type('coachee1@school.org', {force: true});
+
+    // {enter} causes the form to submit
+    cy.get('input[id=password]').type(`YouC0ach{enter}`, {force: true});
+
+    // we should be redirected to /dashboard
+    cy.url().should('include', '/profile');
+
+    // our auth cookie should be present
+    cy.window()
+      .its("sessionStorage")
+      .invoke("getItem", "jwt_token")
+      .should("exist");
+    cy.window()
+      .its("sessionStorage")
+      .invoke("getItem", "username")
+      .should("equal", 'coachee1@school.org');
+  })
+})
+
+describe('Registering a new Customer', () => {
+  it('should be able to register when all fields are filled in.', () => {
+    cy.visit('/');
+
+    cy.get('a[routerLink*="/register"]').contains('Register').click();
+
+    cy.get('input[id="first_name"]').type('test', {force: true});
+    cy.get('input[id="last_name"]').type('test', {force: true});
+    cy.get('input[id="email"]').type('test@email.com', {force: true});
+    cy.get('input[id="password"]').type('Abcdefg1', {force: true});
+    cy.get('input[id="passwordVerification"]').type('Abcdefg1', {force: true});
+
+    cy.get('button[type="submit"]').click();
+  });
+})
